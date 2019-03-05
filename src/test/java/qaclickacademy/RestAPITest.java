401305b358b03f988cce92b62b7ddc2cbc3f94a2
sowmya <sowmya.swamy16@gmail.com>
2019-03-05 11:49:04 -0700

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void twitterDelete()
+	{
+		System.out.println("twitterDelete");
+	}
+
+}
